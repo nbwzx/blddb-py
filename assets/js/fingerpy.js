3926bde8922be493177528126a3d31feb50337ed
nbwzx <nbwzx@126.com>
2023-01-24 13:19:48 +0800

    update

--- assets/js/fingerpy.js
+++ assets/js/fingerpy.js
@@ -1,80 +1,10 @@
 "use strict";
 
-function translate(s1, positionr, positionl) {
-    let fingerrposition = positionr;
-    let fingerlposition = positionl;
-    const arr = s1.split(" ");
-    for (let i = 0; i <= arr.length - 1; i++) {
-        if (fingerlposition === 1 && arr[i][0] === "U") {
-            return 0;
-        }
-        if (fingerlposition !== 2 && fingerrposition !== 2 && arr[i][0] === "U") {
-            return 0;
-        }
-        if (arr[i] === "R") {
-            fingerrposition = fingerrposition + 1;
-        }
-        if (arr[i] === "R'") {
-            fingerrposition = fingerrposition - 1;
-        }
-        if (arr[i] === "R2") {
-            if (fingerrposition === 1) {
-                fingerrposition = 3;
-            } else {
-                return 0;
-            }
-        }
-        if (arr[i] === "R2'") {
-            if (fingerrposition === 3) {
-                fingerrposition = 1;
-            } else {
-                return 0;
-            }
-        }
-        if (fingerrposition === 4 || fingerrposition === 0) {
-            return 0;
-        }
-        if (arr[i] === "L'") {
-            fingerlposition = fingerlposition + 1;
-        }
-        if (arr[i] === "L") {
-            fingerlposition = fingerlposition - 1;
-        }
-        if (arr[i] === "L2'") {
-            if (fingerlposition === 1) {
-                fingerlposition = 3;
-            } else {
-                return 0;
-            }
-        }
-        if (arr[i] === "L2") {
-            if (fingerlposition === 3) {
-                fingerlposition = 1;
-            } else {
-                return 0;
-            }
-        }
-        if (fingerlposition === 4 || fingerlposition === 0) {
-            return 0;
-        }
-    }
-    if (fingerrposition === 4 || fingerrposition === 0) {
-        return 0;
-    }
-    if (fingerlposition === 4 || fingerlposition === 0) {
-        return 0;
-    }
-    return 1;
-}
-
 function finger(s1, positionr, positionl) {
     let fingerrposition = positionr;
     let fingerlposition = positionl;
     const arr = s1.split(" ");
     for (let i = 0; i <= arr.length - 1; i++) {
-        if (fingerlposition === 1 && arr[i][0] === "U") {
-            return 0;
-        }
         if (fingerlposition !== 2 && fingerrposition !== 2 && arr[i][0] === "U") {
             return 0;
         }
